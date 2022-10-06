b6af331472d967e0788118b0af28e8e0c2f864af
manuel londono <mLondono@q10.com>
2022-10-05 20:32:43 -0500

    chore: settings eslint and jsconfig

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
+import '#/styles/globals.css'
+import '#/styles/normalize.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { createUser } from '#/services/createUser'
+
+/**
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ */
+export default function handler (req, res) {
+  const b = createUser()
+
+  res.status(200).json({ name: b })
 }
