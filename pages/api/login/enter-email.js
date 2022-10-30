abe297ec4e2df8340e7dc0298740b99ea59a0b35
manuel londono <mLondono@q10.com>
2022-10-30 09:44:03 -0500

    fix: better linting

--- pages/api/login/enter-email.js
+++ pages/api/login/enter-email.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { enterEmail } from '#/services/Login/EnterEmail'
+import { enterEmail } from '#/services/Login/enterEmail'
 
 /**
  * @param {import('next').NextApiRequest} req
