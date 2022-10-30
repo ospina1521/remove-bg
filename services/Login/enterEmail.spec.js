89b87f4b5dabc5aadb986431da3ee7da20bdcb50
manuel londono <mLondono@q10.com>
2022-10-30 09:20:52 -0500

    build: test it.concurrent

--- services/Login/enterEmail.spec.js
+++ services/Login/enterEmail.spec.js
@@ -7,33 +7,33 @@ describe('EnterEmail API', () => {
   })
 
   it('should throw if email parameter is not provided', () => {
-    // // @ts-ignore
-    // expect(() => enterEmail())
-    //   .rejects
-    //   .toThrow('Email parameter is required')
+    // @ts-ignore
+    expect(() => enterEmail())
+      .rejects
+      .toThrow('Email parameter is required')
   })
 
-  // it.concurrent('should throw if email parameter is 123', () => {
-  //   expect(() => enterEmail('123'))
-  //     .rejects
-  //     .toThrow('Email parameter is not valid')
-  // })
+  it.concurrent('should throw if email parameter is 123', () => {
+    expect(() => enterEmail('123'))
+      .rejects
+      .toThrow('Email parameter is not valid')
+  })
 
-  // it.concurrent('should throw if email parameter is 123@', () => {
-  //   expect(() => enterEmail('123@'))
-  //     .rejects
-  //     .toThrow('Email parameter is not valid')
-  // })
+  it.concurrent('should throw if email parameter is 123@', () => {
+    expect(() => enterEmail('123@'))
+      .rejects
+      .toThrow('Email parameter is not valid')
+  })
 
-  // it.concurrent('should throw if email parameter is abc@abc', () => {
-  //   expect(() => enterEmail('abc@abc'))
-  //     .rejects
-  //     .toThrow('Email parameter is not valid')
-  // })
+  it.concurrent('should throw if email parameter is abc@abc', () => {
+    expect(() => enterEmail('abc@abc'))
+      .rejects
+      .toThrow('Email parameter is not valid')
+  })
 
-  // it.concurrent('should throw if email parameter is abc@abc.c', () => {
-  //   expect(() => enterEmail('abc@abc.c'))
-  //     .rejects
-  //     .toThrow('Email parameter is not valid')
-  // })
+  it.concurrent('should throw if email parameter is abc@abc.c', () => {
+    expect(() => enterEmail('abc@abc.c'))
+      .rejects
+      .toThrow('Email parameter is not valid')
+  })
 })
